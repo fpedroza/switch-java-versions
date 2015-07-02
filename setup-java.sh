6f2af3199652b20630898e43402ba8eda76656d4
Frank Pedroza <f.pedroza@elsevier.com>
2015-07-02 15:16:35 -0600

    add the script

---
+++ setup-java.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# setup aliases necessary to switch active java version
+
+export JAVA_8_HOME=$(/usr/libexec/java_home -v1.8)
+export JAVA_7_HOME=$(/usr/libexec/java_home -v1.7)
+
+alias java7='export JAVA_HOME=$JAVA_7_HOME'  # switch to java 7
+alias java8='export JAVA_HOME=$JAVA_8_HOME'  # switch to java 8
+
+export JAVA_HOME=$JAVA_8_HOME
